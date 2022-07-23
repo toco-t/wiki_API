f05905d4a3aa3c1cf96c1182d96b1be1dbe07d49
Toco Tachibana <ttachibana@my.bcit.ca>
2022-07-23 00:54:08 -0700

    Connect to mongoDB and create schema and collection

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = new mongoose.schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.listen("3000", (req, res) => {
   console.log("Server is running on port 3000...");
 });
